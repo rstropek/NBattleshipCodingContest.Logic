890cfc868e9cd016d872cee9f34a6d67771ec411
Rainer Stropek <rainer@software-architects.at>
2021-03-31 07:49:23 +0200

    Switch library to .NET Standard 2.1

--- NBattleshipCodingContest.Logic/Model.cs
+++ NBattleshipCodingContest.Logic/Model.cs
@@ -1,5 +1,18 @@
 ﻿using System;
 
+namespace System.Runtime.CompilerServices
+{
+    using System.ComponentModel;
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
+
 namespace NBattleshipCodingContest.Logic
 {
     // Note the use of records here. Read more at
